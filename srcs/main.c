3a5c4b726b6cb35231316d17f4b66a10dfdedb8a
Hugo SPRIET <hspriet@e1r6p19.clusters.42paris.fr>
2023-01-16 14:01:41 +0100

    add map

--- srcs/main.c
+++ srcs/main.c
@@ -1,16 +1,44 @@
 #include "../includes/cub.h"
 
+void print_map(char **map)
+{
+	int i = 0;
+	int j = 0;
+
+	while (map[j])
+	{
+		i = 0;
+		while (map[j][i])
+		{
+			printf("%c", map[j][i]);
+			i++;
+		}
+		printf("\n");
+		j++;
+	}
+}
+
+
 int	main(int ac, char **av)
 {
-	t_game *game;
+	t_game	*game;
+	char	*str;
 
-	(void) ac;
-	(void) av;
-	game = malloc((sizeof (t_game)));
 	if (ac != 2)
-		parse_error();
-	game->mlx = mlx_init();
-	if (game->mlx == NULL)
-		parse_error();
-	parse(av[1], game);
+	{
+		write(2, "Error\n", 6);
+		return (0);
+	}
+	game = malloc(sizeof(t_game));
+	struct_init(game, av[1]);
+	str = parse(game);
+	game->tab = ft_split(str, '\n');
+
+	free(str);
+	//init_img(game, game->tab);
+	//print_tiles(game, game->tab);
+	//mlx_loop(game->mlx);
+	print_map(game ->tab);
+	free(game);
+	return (0);
 } 
